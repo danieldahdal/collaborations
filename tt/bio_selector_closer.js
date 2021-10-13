666ef5f4364dbcde45273cdaa6f36f18d64018f0
danieldahdal <89171136+danieldahdal@users.noreply.github.com>
2021-10-13 04:56:24 -0500

    Update bio_selector_closer.js

--- tt/bio_selector_closer.js
+++ tt/bio_selector_closer.js
@@ -5,44 +5,6 @@ function showMenu(){
 var menu = $( "#bio_selector" );
 var spanTags = $( "span" );
 
-if(menu.css("display") == "block"){
-
-//Start of animation
-menu.animate(
-{
-height: "toggle"
-}, 300);
-
-spanTags.eq( 0 ).css("transition", "0.3s");
-spanTags.eq( 0 ).css("margin-top", "0px");
-spanTags.eq( 0 ).css("-moz-transform", "rotate(0deg)");
-spanTags.eq( 0 ).css("-ms-transform", "rotate(0deg)");
-spanTags.eq( 0 ).css("-o-transform", "rotate(0deg)");
-spanTags.eq( 0 ).css("-webkit-transform", "rotate(0deg)");
-spanTags.eq( 0 ).css("transform", "rotate(0deg)");
-
-spanTags.eq( 1 ).css("transition", "0.3s");
-spanTags.eq( 1 ).css("display", "block");
-
-spanTags.eq( 2 ).css("transition", "0.3s");
-spanTags.eq( 2 ).css("margin-top", "0px");
-spanTags.eq( 2 ).css("-moz-transform", "rotate(0deg)");
-spanTags.eq( 2 ).css("-ms-transform", "rotate(0deg)");
-spanTags.eq( 2 ).css("-o-transform", "rotate(0deg)");
-spanTags.eq( 2 ).css("-webkit-transform", "rotate(0deg)");
-spanTags.eq( 2 ).css("transform", "rotate(0deg)");
-//End of animation
-
-}
-else
-{
-
-//Start of animation
-menu.animate(
-{
-height: "toggle"
-}, 300);
-
 spanTags.eq( 0 ).css("transition", "0.3s");
 spanTags.eq( 0 ).css("margin-top", "9px");
 spanTags.eq( 0 ).css("-moz-transform", "rotate(45deg)");
@@ -61,6 +23,14 @@ spanTags.eq( 2 ).css("-ms-transform", "rotate(-45deg)");
 spanTags.eq( 2 ).css("-o-transform", "rotate(-45deg)");
 spanTags.eq( 2 ).css("-webkit-transform", "rotate(-45deg)");
 spanTags.eq( 2 ).css("transform", "rotate(-45deg)");
+
+if(menu.css("display") == "block"){
+
+//Start of animation
+menu.animate(
+{
+height: "toggle"
+}, 300);
 //End of animation
 
 }
